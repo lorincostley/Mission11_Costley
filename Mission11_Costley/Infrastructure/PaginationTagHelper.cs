eaac277d836f73f62b37b65d15d72bef8926f530
lorincostley <lorin.costley@gmail.com>
2024-03-20 10:34:32 -0600

    todo

--- Mission11_Costley/Infrastructure/PaginationTagHelper.cs
+++ Mission11_Costley/Infrastructure/PaginationTagHelper.cs
@@ -21,6 +21,12 @@ namespace Mission11_Costley.Infrastructure
         public ViewContext? ViewContext { get; set; }
         public string? PageAction { get; set; }
         public PaginationInfo PageModel { get; set; }
+
+        public bool PageClassesEnabled { get; set; } = false;
+        public string PageClass { get; set; } = String.Empty;
+        public string PageClassNormal { get; set; } = String.Empty;
+        public string PageClassSelected {  get; set; } = String.Empty;
+
         public override void Process(TagHelperContext context, TagHelperOutput output)
         {
             if (ViewContext != null && PageModel != null) 
@@ -34,6 +40,12 @@ namespace Mission11_Costley.Infrastructure
                     TagBuilder tag = new TagBuilder("a");
 
                     tag.Attributes["href"] = urlHelper.Action(PageAction, new {pageNum = i});
+
+                    if(PageClassesEnabled)
+                    {
+                        tag.AddCssClass(PageClass);
+                        tag.AddCssClass(i == PageModel.CurrentPage ? PageClassSelected : PageClassNormal);
+                    }
 
                     tag.InnerHtml.Append(i.ToString());
 
